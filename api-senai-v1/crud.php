4d2a9ab3d95d7faa6c266031a78218117bf6b26e
brunalozinski <lozinskibruna@gmail.com>
2021-11-05 11:08:29 -0300

    save

--- api-senai-v1/crud.php
+++ api-senai-v1/crud.php
@@ -19,6 +19,22 @@
         }
     }
 
+    // FUNÇÃO DE LEITURA DE DADOS (SEM CRITÉRIOS)
+    function readID($cod_pessoa, $conn) {
+        $sql = "SELECT * FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+        if($resultado = mysqli_query($conn, $sql)) {
+
+            $dados = mysqli_fetch_all($resultado);
+
+            echo json_encode(array("status"=>"success", "data"=>$dados));
+
+        } else {
+            echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
+
+        }
+    }
+
     // FUNÇÃO DE INSERÇÃO
     function create($nome, $sobrenome, $email, $celular, $fotografia, $conn) {
         
@@ -33,5 +49,37 @@
 
 
     }
+
+    // FUNÇÃO DE ATUALIZAÇÃO
+    function update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn) {
+
+        $sql = "UPDATE tbl_pessoa SET
+                nome = '$nome',
+                sobrenome = '$sobrenome',
+                email = '$email',
+                celular = '$celular', 
+                fotografia = '$fotografia'
+                WHERE cod_pessoa = $cod_pessoa";
+
+        if (mysqli_query($conn, $sql)) {
+            echo json_encode(array("status"=>"success", "data"=>"Dados alterados com sucesso"));
+        } else {
+            echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
+        }
+    }
+
+    // FUNÇÃO DE EXCLUSÃO
+    function delete($cod_pessoa, $conn) {
+        $sql = "DELETE FROM tbl_pessoa WHERE cod_pessoa = $cod_pessoa";
+
+        if (mysqli_query($conn, $sql)) {
+            echo json_encode(array("status"=>"success", "data"=>"Dados excluídos com sucesso"));
+        } else {
+            echo json_encode(array("status"=>"error", "data"=>mysqli_error($conn)));
+        }
+
+    }
+
+
 
 ?>
--- api-senai-v1/index.php
+++ api-senai-v1/index.php
@@ -20,6 +20,13 @@
 
     }
 
+    if ($acao == "readID") {
+
+        $cod_pessoa = $_REQUEST["cod_pessoa"];
+        readID($cod_pessoa, $conn);
+
+    }
+
     // ROTA DO CREATE
     if ($acao == "create") {
 
@@ -31,6 +38,26 @@
 
         create($nome, $sobrenome, $email, $celular, $fotografia, $conn);
 
+    }
+
+    // ROTA DO UPDATE
+    if ($acao == "update") {
+
+        $cod_pessoa = $_REQUEST["cod_pessoa"];
+        $nome = $_REQUEST["nome"];
+        $sobrenome = $_REQUEST["sobrenome"];
+        $email = $_REQUEST["email"];
+        $celular = $_REQUEST["celular"];
+        $fotografia = $_REQUEST["fotografia"];
+
+        update($cod_pessoa, $nome, $sobrenome, $email, $celular, $fotografia, $conn);
+
+    }
+
+    // ROTA DO DELETE
+    if ($acao == "delete") {
+        $cod_pessoa = $_REQUEST["cod_pessoa"];
+        delete($cod_pessoa, $conn);
     }
 
 ?>
